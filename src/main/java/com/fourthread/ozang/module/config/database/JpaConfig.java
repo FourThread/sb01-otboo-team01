789339be0ba62580a6d3ff52a8eb6a9ad7f4dddb
TaeHyoun Kim <kth9903@gmail.com>
2025-06-23 23:36:25 +0900

    init: ci 및 구조 초기 세팅

---
+++ src/main/java/com/fourthread/ozang/module/config/database/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.fourthread.ozang.module.config.database;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
