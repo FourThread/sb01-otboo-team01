fb373b102e42850b2f80b135ba2680b74600c0d4
reflash407 <reflash407@naver.com>
2025-06-23 15:52:00 +0900

    setup

---
+++ src/test/java/com/fourthread/ozang/OZangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourthread.ozang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OZangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
