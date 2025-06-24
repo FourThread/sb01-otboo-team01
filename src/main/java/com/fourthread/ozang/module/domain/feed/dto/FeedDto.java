b1d44d6f4b499f8db3e8f979587ce39d5d2c19c3
wongil <lee.cocaine@gmail.com>
2025-06-24 09:58:56 +0900

    feat: base entity

---
+++ src/main/java/com/fourthread/ozang/module/domain/feed/dto/FeedDto.java
@@ -0,0 +1,9 @@
+package com.fourthread.ozang.module.domain.feed.dto;
+
+import com.fourthread.ozang.module.domain.BaseUpdatableEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class FeedDto extends BaseUpdatableEntity {
+
+}
