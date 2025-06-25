9a8790a479814b92a40338f8c6dafd8a583e640e
reflash407 <reflash407@naver.com>
2025-06-25 15:13:13 +0900

    feat : ClothesAttributeDefinitionService update 추가

--- src/main/java/com/fourthread/ozang/module/domain/clothes/service/ClothesAttributeDefinitionService.java
+++ src/main/java/com/fourthread/ozang/module/domain/clothes/service/ClothesAttributeDefinitionService.java
@@ -2,6 +2,7 @@ package com.fourthread.ozang.module.domain.clothes.service;
 
 
 import com.fourthread.ozang.module.domain.clothes.dto.requeset.ClothesAttributeDefCreateRequest;
+import com.fourthread.ozang.module.domain.clothes.dto.requeset.ClothesAttributeDefUpdateRequest;
 import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesAttributeDefDto;
 import com.fourthread.ozang.module.domain.clothes.entity.ClothesAttributeDefinition;
 import com.fourthread.ozang.module.domain.clothes.mapper.ClothesAttributeDefinitionMapper;
@@ -9,16 +10,35 @@ import com.fourthread.ozang.module.domain.clothes.repository.ClothesAttributeDef
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class ClothesAttributeDefinitionService {
 
-    private final ClothesAttributeDefinitionRepository repository;
-    private final ClothesAttributeDefinitionMapper mapper;
+    private final ClothesAttributeDefinitionRepository definitionRepository;
+    private final ClothesAttributeDefinitionMapper definitionMapper;
 
     public ClothesAttributeDefDto create(ClothesAttributeDefCreateRequest request) {
+        if (definitionRepository.existsByName(request.name())) {
+            throw new IllegalArgumentException("이미 존재하는 속성 이름입니다."); //TODO 커스텀 예외처리
+        }
         ClothesAttributeDefinition clothesAttributeDefinition = new ClothesAttributeDefinition(request.name(), request.selectableValues());
-        ClothesAttributeDefinition save = repository.save(clothesAttributeDefinition);
-        return mapper.toDto(save);
+        ClothesAttributeDefinition save = definitionRepository.save(clothesAttributeDefinition);
+        return definitionMapper.toDto(save);
     }
+
+    public ClothesAttributeDefDto update(UUID definitionId, ClothesAttributeDefUpdateRequest request) {
+        ClothesAttributeDefinition definition = definitionRepository.findById(definitionId)
+                .orElseThrow(() -> new IllegalArgumentException("속성 정의가 존재하지 않습니다.")); //TODO 커스텀 예외처리
+
+        if (definitionRepository.existsByNameAndIdNot(request.name(), definitionId)) {
+            throw new IllegalArgumentException("해당 속성정의 이름이 이미 존재합니다."); //TODO 커스텀 예외처리
+        }
+
+        definition.update(request.name(), request.selectableValues());
+        return definitionMapper.toDto(definition);
+    }
+
+
 }
