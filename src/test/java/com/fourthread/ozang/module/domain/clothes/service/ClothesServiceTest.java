31cd1614a9c941e45ae59661145b5d3e5bc39203
reflash407 <reflash407@naver.com>
2025-06-30 18:54:00 +0900

    feat : ClothesServiceTest update 성공, 실패 테스트 추가

--- src/test/java/com/fourthread/ozang/module/domain/clothes/service/ClothesServiceTest.java
+++ src/test/java/com/fourthread/ozang/module/domain/clothes/service/ClothesServiceTest.java
@@ -1,6 +1,7 @@
 package com.fourthread.ozang.module.domain.clothes.service;
 
 import com.fourthread.ozang.module.domain.clothes.dto.requeset.ClothesCreateRequest;
+import com.fourthread.ozang.module.domain.clothes.dto.requeset.ClothesUpdateRequest;
 import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesAttributeDto;
 import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesAttributeWithDefDto;
 import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesDto;
@@ -33,9 +34,13 @@ import static org.mockito.Mockito.never;
 @ExtendWith(MockitoExtension.class)
 class ClothesServiceTest {
 
+    private UUID clothesId;
     private UUID ownerId;
     private UUID definitionId;
     private ClothesAttributeDefinition definition;
+    private Clothes existingClothes;
+    private ClothesDto updatedDto;
+
 
     private ClothesDto clothesDto;
 
@@ -53,12 +58,32 @@ class ClothesServiceTest {
 
     @BeforeEach
     void setUp() {
+        clothesId = UUID.randomUUID();
         ownerId = UUID.randomUUID();
         definitionId = UUID.randomUUID();
 
         definition = new ClothesAttributeDefinition("컬러", List.of("화이트", "블랙"));
         ReflectionTestUtils.setField(definition, "id", definitionId);
 
+        existingClothes = Clothes.builder()
+                .ownerId(ownerId)
+                .name("기존 이름")
+                .type(ClothesType.TOP)
+                .build();
+        ReflectionTestUtils.setField(existingClothes, "id", clothesId);
+
+        updatedDto = new ClothesDto(
+                clothesId,
+                ownerId,
+                "수정된 셔츠",
+                null, // 이미지 없음
+                ClothesType.TOP,
+                List.of(new ClothesAttributeWithDefDto(
+                        definitionId,
+                        "컬러",
+                        List.of("화이트", "블랙"),
+                        "블랙"))
+        );
 
         clothesDto = new ClothesDto(
                 UUID.randomUUID(),
@@ -120,4 +145,58 @@ class ClothesServiceTest {
 
         then(clothesRepository).should(never()).save(any());
     }
+
+    @DisplayName("의상을 수정할 수 있다")
+    @Test
+    void update_success() {
+        //given
+        ClothesAttributeDto attrDto = new ClothesAttributeDto(definitionId, "블랙");
+        ClothesUpdateRequest request = new ClothesUpdateRequest("수정된 셔츠", ClothesType.TOP, List.of(attrDto));
+
+        given(clothesRepository.findById(clothesId)).willReturn(Optional.of(existingClothes));
+        given(definitionRepository.findById(definitionId)).willReturn(Optional.of(definition));
+        given(clothesMapper.toDto(any())).willReturn(updatedDto);
+
+        //when
+        ClothesDto result = clothesService.update(clothesId, request, null);
+
+        //then
+        assertThat(result).isEqualTo(updatedDto);
+        then(clothesRepository).should(never()).save(any());
+    }
+
+    @DisplayName("속성 정의 ID가 존재하지 않으면 수정에 실패한다")
+    @Test
+    void update_fail_definition_not_found() {
+        //given
+        UUID invalidDefId = UUID.randomUUID();
+        ClothesUpdateRequest request = new ClothesUpdateRequest("수정된 셔츠", ClothesType.TOP,
+                List.of(new ClothesAttributeDto(invalidDefId, "블랙")));
+
+        given(clothesRepository.findById(clothesId)).willReturn(Optional.of(existingClothes));
+        given(definitionRepository.findById(invalidDefId)).willReturn(Optional.empty());
+
+        //when then
+        assertThatThrownBy(() -> clothesService.update(clothesId, request, null))
+                .isInstanceOf(IllegalArgumentException.class);
+        then(clothesRepository).should(never()).save(any());
+    }
+
+
+    @DisplayName("의상 ID가 존재하지 않으면 수정에 실패한다.")
+    @Test
+    void update_fail_clothes_not__found() {
+        //given
+        ClothesUpdateRequest request = new ClothesUpdateRequest("수정된 셔츠", ClothesType.TOP,
+                List.of(new ClothesAttributeDto(definitionId, "블랙")));
+
+        given(clothesRepository.findById(clothesId)).willReturn(Optional.empty());
+
+        //when then
+        assertThatThrownBy(() -> clothesService.update(clothesId, request, null))
+                .isInstanceOf(IllegalArgumentException.class);
+        then(definitionRepository).should(never()).findById(any());
+    }
+
+
 }
