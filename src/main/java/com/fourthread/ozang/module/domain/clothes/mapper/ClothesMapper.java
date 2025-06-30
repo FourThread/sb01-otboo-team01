ef1ba68fe84d04ef09e43145f15f97de124a7a29
reflash407 <reflash407@naver.com>
2025-06-30 15:15:10 +0900

    feat : ClothesMapper toDt 추가

--- src/main/java/com/fourthread/ozang/module/domain/clothes/mapper/ClothesMapper.java
+++ src/main/java/com/fourthread/ozang/module/domain/clothes/mapper/ClothesMapper.java
@@ -1,4 +1,36 @@
 package com.fourthread.ozang.module.domain.clothes.mapper;
 
+import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesAttributeWithDefDto;
+import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesDto;
+import com.fourthread.ozang.module.domain.clothes.entity.Clothes;
+import com.fourthread.ozang.module.domain.clothes.entity.ClothesAttribute;
+import com.fourthread.ozang.module.domain.clothes.entity.ClothesAttributeDefinition;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClothesMapper {
+
+    public ClothesDto toDto(Clothes clothes) {
+        return new ClothesDto(
+                clothes.getId(),
+                clothes.getOwnerId(),
+                clothes.getName(),
+                null, //TODO 이미지 처리
+                clothes.getType(),
+                clothes.getAttributes().stream()
+                        .map(this::toAttributeWithDefDto)
+                        .toList()
+        );
+    }
+
+    private ClothesAttributeWithDefDto toAttributeWithDefDto(ClothesAttribute attr) {
+        ClothesAttributeDefinition def = attr.getDefinition();
+
+        return new ClothesAttributeWithDefDto(
+                def.getId(),
+                def.getName(),
+                def.getSelectableValues(),
+                attr.getValue()
+        );
+    }
 }
