4ed7786f422a59a095a58f5ffa87a4a3160ec124
reflash407 <reflash407@naver.com>
2025-06-30 15:14:16 +0900

    feat : ClothesCreateRequest, ClothesAttributeDto 검증 추가

--- src/main/java/com/fourthread/ozang/module/domain/clothes/dto/requeset/ClothesCreateRequest.java
+++ src/main/java/com/fourthread/ozang/module/domain/clothes/dto/requeset/ClothesCreateRequest.java
@@ -2,14 +2,28 @@ package com.fourthread.ozang.module.domain.clothes.dto.requeset;
 
 import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesAttributeDto;
 import com.fourthread.ozang.module.domain.clothes.entity.ClothesType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 import java.util.UUID;
 
 public record ClothesCreateRequest(
+
+        @NotNull(message = "소유자 ID는 필수입니다.")
         UUID ownerId,
+
+        @NotBlank(message = "이름은 필수 입력값입니다.")
         String name,
+
+        @NotNull(message = "의상 타입은 필수입니다.")
         ClothesType type,
+
+        @NotNull(message = "속성 목록은 필수입니다.")
+        @Size(min = 1, message = "적어도 하나 이상의 속성이 필요합니다.")
+        @Validated
         List<ClothesAttributeDto> attributes
 ) {
 }
