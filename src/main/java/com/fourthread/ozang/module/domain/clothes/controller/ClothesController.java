ba67c55b2ccc0c8e0a3790a0c6cfd0667669de95
reflash407 <reflash407@naver.com>
2025-06-30 15:12:55 +0900

    feat : ClothesController create 추가

---
+++ src/main/java/com/fourthread/ozang/module/domain/clothes/controller/ClothesController.java
@@ -0,0 +1,34 @@
+package com.fourthread.ozang.module.domain.clothes.controller;
+
+import com.fourthread.ozang.module.domain.clothes.dto.requeset.ClothesCreateRequest;
+import com.fourthread.ozang.module.domain.clothes.dto.response.ClothesDto;
+import com.fourthread.ozang.module.domain.clothes.service.ClothesService;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.protocol.HTTP;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/clothes")
+public class ClothesController {
+
+    private final ClothesService clothesService;
+
+    @PostMapping(value = "/api/clothes", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ClothesDto> create(
+            @RequestPart("request") @Validated ClothesCreateRequest request,
+            @RequestPart(value = "image", required = false) MultipartFile imageFile) {
+        ClothesDto response = clothesService.create(request, imageFile);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
