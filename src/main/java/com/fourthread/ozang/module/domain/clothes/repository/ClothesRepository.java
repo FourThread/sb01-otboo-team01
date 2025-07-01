08bbdcdb66d07cbf6159d7be4df5704419a0b74c
reflash407 <reflash407@naver.com>
2025-07-01 17:42:14 +0900

    feat : ClothesRepositoryImpl 쿼리 dsl 레포지터리 추가

--- src/main/java/com/fourthread/ozang/module/domain/clothes/repository/ClothesRepository.java
+++ src/main/java/com/fourthread/ozang/module/domain/clothes/repository/ClothesRepository.java
@@ -1,9 +1,10 @@
 package com.fourthread.ozang.module.domain.clothes.repository;
 
 import com.fourthread.ozang.module.domain.clothes.entity.Clothes;
+import com.fourthread.ozang.module.domain.clothes.repository.query.ClothesRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ClothesRepository extends JpaRepository<Clothes, UUID> {
+public interface ClothesRepository extends JpaRepository<Clothes, UUID>, ClothesRepositoryCustom {
 }
