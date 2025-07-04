af77d5a1ed4083ceff9d3906b8fef8e46d5b3911
wongil <lee.cocaine@gmail.com>
2025-07-04 16:25:23 +0900

    feat: retrieve direct message

--- src/main/java/com/fourthread/ozang/module/domain/dm/controller/DirectMessageController.java
+++ src/main/java/com/fourthread/ozang/module/domain/dm/controller/DirectMessageController.java
@@ -2,13 +2,18 @@ package com.fourthread.ozang.module.domain.dm.controller;
 
 import com.fourthread.ozang.module.domain.dm.dto.DirectMessageCreateRequest;
 import com.fourthread.ozang.module.domain.dm.dto.DirectMessageDto;
+import com.fourthread.ozang.module.domain.dm.dto.DirectMessageDtoCursorRequest;
+import com.fourthread.ozang.module.domain.dm.dto.DirectMessageDtoCursorResponse;
 import com.fourthread.ozang.module.domain.dm.service.DirectMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class DirectMessageController {
 
@@ -18,5 +23,13 @@ public class DirectMessageController {
   public DirectMessageDto send(@Payload DirectMessageCreateRequest request) {
 
     return dmMessageService.send(request);
+  }
+
+  @GetMapping("/api/direct-messages")
+  public DirectMessageDtoCursorResponse retrieveDirectMessage(
+      @Validated @ModelAttribute DirectMessageDtoCursorRequest request) {
+
+    return dmMessageService.retrieve(request);
+
   }
 }
