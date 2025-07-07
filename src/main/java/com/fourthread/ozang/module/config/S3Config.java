575c247c7e2b33fcf9153d5f981e780a4c8d39cc
TaeHyoun Kim <kth9903@gmail.com>
2025-07-07 14:30:07 +0900

    feat: AWS S3 클라이언트 설정 (테스트 환경 제외)

--- src/main/java/com/fourthread/ozang/module/config/S3Config.java
+++ src/main/java/com/fourthread/ozang/module/config/S3Config.java
@@ -1,33 +1,38 @@
-//package com.fourthread.ozang.module.config;
-//
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class S3Config {
-//
-//  @Value("${sb01-otboo-team01.storage.s3.access-key}")
-//  private String accessKey;
-//
-//  @Value("${sb01-otboo-team01.storage.s3.secret-key}")
-//  private String secretKey;
-//
-//  @Value("${sb01-otboo-team01.storage.s3.region}")
-//  private String region;
-//
-//  @Bean
-//  public AmazonS3 amazonS3Client() {
-//    BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-//
-//    return AmazonS3ClientBuilder.standard()
-//        .withRegion(region)
-//        .withCredentials(new AWSStaticCredentialsProvider(credentials))
-//        .build();
-//  }
-//
-//}
+package com.fourthread.ozang.module.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@Profile("!test")  // 테스트 환경이 아닐 때만 활성화
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .build();
+    }
+}
