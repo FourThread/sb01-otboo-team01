b8e712c8a4797965e3b4aac56d813207e4fa290e
normaldeve <junnukim1007@gmail.com>
2025-07-07 17:54:00 +0900

    feat : 이름 변경

--- src/main/java/com/fourthread/ozang/module/domain/clothes/service/ImageService.java
+++ src/main/java/com/fourthread/ozang/module/domain/clothes/service/ImageService.java
@@ -9,11 +9,11 @@ public interface ImageService {
    * @param file 업로드할 이미지 파일
    * @return 업로드된 이미지 URL
    */
-  String uploadClothesImage(MultipartFile file);
+  String uploadImage(MultipartFile file);
 
   /**
    * 의상 이미지를 삭제
    * @param imageUrl 삭제할 이미지 URL
    */
-  void deleteClothesImage(String imageUrl);
+  void deleteImage(String imageUrl);
 }
