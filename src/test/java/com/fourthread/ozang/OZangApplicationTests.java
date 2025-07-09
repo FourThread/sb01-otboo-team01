dbee6bce121f67fd74a11d4e17d096eda67952f6
wongil <lee.cocaine@gmail.com>
2025-07-09 17:15:51 +0900

    fix: fix test

--- src/test/java/com/fourthread/ozang/OZangApplicationTests.java
+++ src/test/java/com/fourthread/ozang/OZangApplicationTests.java
@@ -3,23 +3,12 @@ package com.fourthread.ozang;
 import com.fourthread.ozang.module.domain.feed.elasticsearch.repository.FeedElasticsearchRepository;
 import com.fourthread.ozang.module.domain.feed.elasticsearch.service.FeedSearchService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration,org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration,org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration"
-})
-@EnableAutoConfiguration(exclude = {
-    ElasticsearchDataAutoConfiguration.class,
-    ElasticsearchRestClientAutoConfiguration.class
-})
 class OZangApplicationTests {
 
     @MockitoBean
