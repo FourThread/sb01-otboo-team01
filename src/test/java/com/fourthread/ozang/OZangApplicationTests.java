53a049b5465e6e2ef177e15ea474057fcc9615fc
wongil <lee.cocaine@gmail.com>
2025-07-09 16:40:58 +0900

    fix: fix test

--- src/test/java/com/fourthread/ozang/OZangApplicationTests.java
+++ src/test/java/com/fourthread/ozang/OZangApplicationTests.java
@@ -1,10 +1,10 @@
 package com.fourthread.ozang;
 
-import com.fourthread.ozang.module.config.TestConfig;
+import com.fourthread.ozang.module.domain.feed.elasticsearch.repository.FeedElasticsearchRepository;
+import com.fourthread.ozang.module.domain.feed.elasticsearch.service.FeedSearchService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
@@ -13,9 +13,13 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(properties = {
     "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration,org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration,org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration"
 })
-@ConditionalOnProperty(name = "elasticsearch.enabled", havingValue = "true", matchIfMissing = true)
-@Import(TestConfig.class)
 class OZangApplicationTests {
+
+    @Mock
+    private FeedElasticsearchRepository feedElasticsearchRepository;
+
+    @Mock
+    private FeedSearchService feedSearchService;
 
     @Test
     void contextLoads() {
