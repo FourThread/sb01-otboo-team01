95f3e4ef7f57eccd12e487d367ed07629371c8e4
wongil <lee.cocaine@gmail.com>
2025-07-09 17:54:49 +0900

    fix: fix test

--- src/test/java/com/fourthread/ozang/OZangApplicationTests.java
+++ src/test/java/com/fourthread/ozang/OZangApplicationTests.java
@@ -1,21 +1,15 @@
 package com.fourthread.ozang;
 
-import com.fourthread.ozang.module.domain.feed.elasticsearch.repository.FeedElasticsearchRepository;
-import com.fourthread.ozang.module.domain.feed.elasticsearch.service.FeedSearchService;
+import com.fourthread.ozang.module.domain.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestConfig.class)
 class OZangApplicationTests {
-
-    @MockitoBean
-    private FeedElasticsearchRepository feedElasticsearchRepository;
-
-    @MockitoBean
-    private FeedSearchService feedSearchService;
 
     @Test
     void contextLoads() {
--- src/test/java/com/fourthread/ozang/module/domain/config/TestConfig.java
+++ src/test/java/com/fourthread/ozang/module/domain/config/TestConfig.java
@@ -3,17 +3,17 @@ package com.fourthread.ozang.module.domain.config;
 import com.fourthread.ozang.module.domain.feed.elasticsearch.repository.FeedElasticsearchRepository;
 import com.fourthread.ozang.module.domain.feed.elasticsearch.service.FeedSearchService;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@TestConfiguration
+@Configuration
+@Profile("test")
 public class TestConfig {
-
   @Bean
   public FeedElasticsearchRepository feedElasticsearchRepository() {
     return Mockito.mock(FeedElasticsearchRepository.class);
   }
-
   @Bean
   public FeedSearchService feedSearchService() {
     return Mockito.mock(FeedSearchService.class);
