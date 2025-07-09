b37a01b469741fd81601735828c24b8159c8bd6d
wongil <lee.cocaine@gmail.com>
2025-07-09 16:30:29 +0900

    fix: fix test

--- src/test/java/com/fourthread/ozang/OZangApplicationTests.java
+++ src/test/java/com/fourthread/ozang/OZangApplicationTests.java
@@ -1,10 +1,6 @@
 package com.fourthread.ozang;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -13,11 +9,6 @@ import org.springframework.test.context.TestPropertySource;
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
     "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration,org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration,org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration"
-})
-@SpringBootApplication(exclude = {
-    ElasticsearchDataAutoConfiguration.class,
-    ElasticsearchRepositoriesAutoConfiguration.class,
-    ElasticsearchClientAutoConfiguration.class
 })
 class OZangApplicationTests {
 
