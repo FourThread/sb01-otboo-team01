fd993524255c7d5678605b09b08d5a98559a65af
wongil <lee.cocaine@gmail.com>
2025-07-10 09:38:13 +0900

    fix: fix test

--- src/test/java/com/fourthread/ozang/module/domain/security/SecurityTest.java
+++ src/test/java/com/fourthread/ozang/module/domain/security/SecurityTest.java
@@ -15,11 +15,13 @@ import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,6 +39,11 @@ import org.springframework.transaction.annotation.Transactional;
         "JWT_SECRET=dasd"
     }
 )
+@TestPropertySource(properties = {
+    "spring.data.elasticsearch.repositories.enabled=false",
+    "spring.elasticsearch.enabled=false"
+})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @AutoConfigureMockMvc
 public class SecurityTest {
 
