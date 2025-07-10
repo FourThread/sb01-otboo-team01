833f189caea13e3662f16676c2f437687c4bec9c
reflash407 <reflash407@naver.com>
2025-07-10 14:34:58 +0900

    feat : NotificationEventListener 의상 속성 추가 이벤트, 의상 속성 수정 이벤트 메서드 추가

--- src/main/java/com/fourthread/ozang/module/domain/notification/listener/NotificationEventListener.java
+++ src/main/java/com/fourthread/ozang/module/domain/notification/listener/NotificationEventListener.java
@@ -1,7 +1,10 @@
 package com.fourthread.ozang.module.domain.notification.listener;
 
+import com.fourthread.ozang.module.domain.notification.entity.Notification;
+import com.fourthread.ozang.module.domain.notification.entity.NotificationLevel;
 import com.fourthread.ozang.module.domain.notification.event.*;
 import com.fourthread.ozang.module.domain.notification.service.NotificationService;
+import com.fourthread.ozang.module.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -9,12 +12,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationEventListener {
 
     private final NotificationService notificationService;
+    private final UserRepository userRepository;
 
     // 권한 변경 이벤트
     @Async("eventTaskExecutor")
@@ -27,15 +36,37 @@ public class NotificationEventListener {
     @Async("eventTaskExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(ClothesAttributeAddedEvent event) {
-//        notificationService.create(
-//        ...
-//        );
+
+        Set<UUID> allUserIds = userRepository.findAllUserIds();
+
+        Set<UUID> otherUserIds = allUserIds.stream()
+                .filter(id -> !id.equals(event.ownerId()))
+                .collect(Collectors.toSet());
+
+        notificationService.createAll(
+                otherUserIds,
+                "새로운 의상 속성이 추가되었어요.",
+                String.format("[%s] 의상에 속성이 추가되었습니다.", event.clothesAttributeDefDto().name()),
+                NotificationLevel.INFO
+        );
     }
 
     // 의상 속성 수정 이벤트
     @Async("eventTaskExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(ClothesAttributeUpdatedEvent event) {
+        Set<UUID> allUserIds = userRepository.findAllUserIds();
+
+        Set<UUID> otherUserIds = allUserIds.stream()
+                .filter(id -> !id.equals(event.ownerId()))
+                .collect(Collectors.toSet());
+
+        notificationService.createAll(
+                otherUserIds,
+                "새로운 의상 속성이 변경되었어요.",
+                String.format("[%s] 속성을 확인해보세요.", event.clothesAttributeDefDto().name()),
+                NotificationLevel.INFO
+        );
 
     }
 
