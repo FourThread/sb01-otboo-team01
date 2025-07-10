046731e129f4251184f514455773c5b3015feb18
reflash407 <reflash407@naver.com>
2025-07-10 11:12:41 +0900

    feat : AsyncConfig TaskExecutor 생성 메서드 추가

---
+++ src/main/java/com/fourthread/ozang/module/domain/notification/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.fourthread.ozang.module.domain.notification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "eventTaskExecutor")
+    public TaskExecutor eventExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("event-task-");
+
+        // 이후 MDC, SecurityContext 보존이 필요할 경우 확장
+
+        executor.initialize();
+        return executor;
+    }
+
+}
