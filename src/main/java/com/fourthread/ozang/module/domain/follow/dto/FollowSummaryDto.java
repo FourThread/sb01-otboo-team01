aa5c746c53d2511e3f58b0d6dc60f775c6cc866e
reflash407 <reflash407@naver.com>
2025-07-12 23:11:47 +0900

    feat : FollowSummaryDto 추가

---
+++ src/main/java/com/fourthread/ozang/module/domain/follow/dto/FollowSummaryDto.java
@@ -0,0 +1,12 @@
+package com.fourthread.ozang.module.domain.follow.dto;
+
+import java.util.UUID;
+
+public record FollowSummaryDto(
+        UUID followeeId,
+        long followerCount,
+        long followingCount,
+        boolean followedByMe,
+        UUID followedByMeId,
+        boolean followingMe
+) {}
