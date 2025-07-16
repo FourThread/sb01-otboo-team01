d51a24b532d9d43494f289630379026d9e881cf6
TaeHyoun Kim <kth9903@gmail.com>
2025-07-17 01:23:13 +0900

    refactor: Redis 직렬화/역직렬화를 위한 어노테이션 추가

--- src/main/java/com/fourthread/ozang/module/domain/weather/dto/WeatherAPILocation.java
+++ src/main/java/com/fourthread/ozang/module/domain/weather/dto/WeatherAPILocation.java
@@ -1,11 +1,28 @@
 package com.fourthread.ozang.module.domain.weather.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record WeatherAPILocation(
-     Double latitude,
-     Double longitude,
-     Integer x,
-     Integer y,
-     List<String> locationNames
-) {}
+    @JsonProperty("latitude") Double latitude,
+    @JsonProperty("longitude") Double longitude,
+    @JsonProperty("x") Integer x,
+    @JsonProperty("y") Integer y,
+    @JsonProperty("locationNames") List<String> locationNames
+) {
+    @JsonCreator
+    public WeatherAPILocation(
+        @JsonProperty("latitude") Double latitude,
+        @JsonProperty("longitude") Double longitude,
+        @JsonProperty("x") Integer x,
+        @JsonProperty("y") Integer y,
+        @JsonProperty("locationNames") List<String> locationNames
+    ) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.x = x;
+        this.y = y;
+        this.locationNames = locationNames;
+    }
+}
