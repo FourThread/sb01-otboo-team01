4bedcf9cb8ed7ccf64f14eb39ddaaf28a737f8f1
reflash407 <reflash407@naver.com>
2025-07-16 18:49:06 +0900

    feat: BAD_REQUEST 에러 코드 추가

--- src/main/java/com/fourthread/ozang/module/common/exception/ErrorCode.java
+++ src/main/java/com/fourthread/ozang/module/common/exception/ErrorCode.java
@@ -8,6 +8,7 @@ public enum ErrorCode {
   // 공통
   INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
   INVALID_REQUEST("INVALID_REQUEST", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
+  BAD_REQUEST("BAD_REQUEST", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
 
   // 사용자 관련
   USER_NOT_FOUND("USER_NOT_FOUND", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
