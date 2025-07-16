feb5b918064b5457c24b8d4d5d01f00ed0bf8799
reflash407 <reflash407@naver.com>
2025-07-16 10:18:27 +0900

    feat: FollowingFeedCreatedEvent 인자 타입 수정

--- src/main/java/com/fourthread/ozang/module/domain/notification/event/FollowingFeedCreatedEvent.java
+++ src/main/java/com/fourthread/ozang/module/domain/notification/event/FollowingFeedCreatedEvent.java
@@ -1,9 +1,9 @@
 package com.fourthread.ozang.module.domain.notification.event;
 
-import com.fourthread.ozang.module.domain.user.entity.User;
+import com.fourthread.ozang.module.domain.user.dto.data.UserSummary;
 
 public record FollowingFeedCreatedEvent(
-        User user,
+        UserSummary userSummary,
         String content
 ) {
 }
