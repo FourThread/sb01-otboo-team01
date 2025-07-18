94844ce06b078c972cee9bf0fbb267a518da1cce
wongil <lee.cocaine@gmail.com>
2025-07-18 14:17:58 +0900

    feat: recommendation Clothes with Open AI

---
+++ src/main/java/com/fourthread/ozang/module/domain/recommend/config/OpenApiWebClientConfig.java
@@ -0,0 +1,29 @@
+package com.fourthread.ozang.module.domain.recommend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class OpenApiWebClientConfig {
+
+  @Value("${openai.api.key}")
+  private String openApiKey;
+
+  @Value("${openai.response.url}")
+  private String openApiUrl;
+
+  @Bean("recommendationClient")
+  public WebClient recommendationClient() {
+    return WebClient.builder()
+        .baseUrl(openApiUrl)
+        .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + openApiKey)
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .codecs(config -> config.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
+        .build();
+  }
+
+}
