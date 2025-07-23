3c04178be657fecc4e28c1c59b0bc40106150ac5
normaldeve <junnukim1007@gmail.com>
2025-07-23 17:47:27 +0900

    feat : 사용자 인증 로직 변경

--- src/main/java/com/fourthread/ozang/module/domain/security/jwt/dto/type/SecurityMatchers.java
+++ src/main/java/com/fourthread/ozang/module/domain/security/jwt/dto/type/SecurityMatchers.java
@@ -5,7 +5,6 @@ public class SecurityMatchers {
   public static final String LOGIN = "/api/auth/sign-in";
   public static final String LOGOUT = "/api/auth/sign-out";
   public static final String H2_CONSOLE = "/h2-console/**";
-  public static final String ME = "/api/auth/me";
   public static final String SIGN_UP = "/api/users";
   public static final String REFRESH = "/api/auth/refresh";
   public static final String OAUTH2 = "/api/oauth2/**";
